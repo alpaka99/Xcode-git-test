d33365c38b6ca43c13a80468a72a0f4ea45e97fb
Lets <ruvan123@naver.com>
2023-03-26 22:42:12 +0900

    Initial Commit

---
+++ Xcode git test/Xcode_git_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  Xcode_git_testApp.swift
+//  Xcode git test
+//
+//  Created by user on 2023/03/26.
+//
+
+import SwiftUI
+
+@main
+struct Xcode_git_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
